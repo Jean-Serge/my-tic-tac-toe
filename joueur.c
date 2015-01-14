3c7ecf1c5916e46744ea4ecb5812bae831b91310
JS <js.monbailly@gmail.com>
2015-01-14 22:09:10 +0100

    Correction d'un problème dans joueurs.c

--- joueur.c
+++ joueur.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "include/joueur.h"
 
@@ -6,4 +7,11 @@ void init_joueur(char *nom, char symb, joueur_t *j)
 {
 	j->symbole = symb;
 	j->nom = nom;
+}
+
+int saisir_coord(unsigned int *col, unsigned int *ligne)
+{
+	printf("Veuillez saisir les coordonnées de la case à marquer sous la forme 'X Y' : \n");
+
+	return 1;
 }
